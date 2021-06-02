f995698ca0716ba05cdcc43d6d3a5b32b2d2a167
Rajat Agrawal <rajatagrawal007.ra@gmail.com>
2021-06-02 23:45:15 +0530

    created isYesNO() function to handle yes/no commands.

--- scripts/speech.py
+++ scripts/speech.py
@@ -14,3 +14,18 @@ def speech():
             print('sorry!! please try again')
             return -1
 # print(speech())
+def isYesNo():
+    import speech_recognition as sr
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('Listening : ... ')
+        audio = r.listen(source,timeout = 10,phrase_time_limit=5)
+        
+        try:
+            text = r.recognize_google(audio)
+            return text
+        except:
+            #print('sorry!! please try again')
+            return "No"
